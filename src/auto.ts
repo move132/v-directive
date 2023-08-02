2845b992926eefb49a9836a22498755b0d3a2105
DenseFog <move11@126.com>
2023-08-02 16:02:07 +0800

    chore: change debounce

--- src/auto.ts
+++ src/auto.ts
@@ -1,4 +1,4 @@
-import {throttle} from '@dense-labs/utils'
+import {debounce} from '@dense-labs/utils'
 function onResize(ev: any, el: any): any {
     const height = document.documentElement.clientHeight - el.offsetTop
     el.style.height = height + 'px'
@@ -7,7 +7,7 @@ export default {
 	mounted(el: any, binding: any) {
         setTimeout(() => {
             onResize(null, el)
-            window.addEventListener('resize', throttle((event: any) => onResize(event, el), 1000))
+            window.addEventListener('resize', debounce((event: any) => onResize(event, el), 150))
         }, 200)
 	},
 	// 元素卸载前也记得清理定时器并且移除监听事件
